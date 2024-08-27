394e637e5ce9abe1f506d70def684334982b4fbc
Faiza-Rashid <137864288+Faiza-Rashid@users.noreply.github.com>
2024-08-27 14:24:12 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+
+document.getElementById("myH1"). textContent = "Hello Faiza";
+document.getElementById("abc"). innerHTML = "Hello Faiza Rashid Adiba";
+type="button"
+onclick="document.getElementById('demo').innerHTML = Date()">
+Click me to display Date and Time.
